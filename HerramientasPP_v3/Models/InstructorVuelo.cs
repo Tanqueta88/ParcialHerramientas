10f7a0e613e8d8212636a9b9a91bff16359ba005
tanqueta88 <oliverprim783@gmail.com>
2022-12-03 18:36:33 -0300

    Version para subir todas las mejoras del primer parcial antes de la integracion con base de datos

---
+++ HerramientasPP_v3/Models/InstructorVuelo.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models
+{  
+    public class Instructor
+    {
+        //[Range(1,10000,ErrorMessage ="El campo debe ser un número entre 1 y 10000")] 
+        //[Required(ErrorMessage ="Debe ingresar el Id")]
+        public int Id{get; set;}
+
+        [Range(1,50000,ErrorMessage ="El campo debe ser un número entre 1 y 5000")]
+        //[Required(ErrorMessage ="Debe ingresar el Legajo de Vuelo")]
+        public int LegajoVuelo {get; set;}
+        
+        //[Required(ErrorMessage ="Debe ingresar el Nombre")]
+        public string Nombre {get; set;}
+
+        //[Required(ErrorMessage ="Debe ingresar el Apellido")]
+        public string Apellido {get; set;}
+
+        //[Required(ErrorMessage ="Debe ingresar el DNI")]
+        public int DNI {get; set;}
+        public DateTime FechaExpedicion {get; set;}
+        [Range(1,3)]
+        public int Experiencia{get; set;}
+        public bool EnActividad {get; set;}
+    }
+}
