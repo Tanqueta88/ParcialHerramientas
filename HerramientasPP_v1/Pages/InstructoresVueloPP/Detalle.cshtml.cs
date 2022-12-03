60f7db976fb2564eb9762d89e469677dca4c2f76
Jorge Olivera <oliver_prim@hotmail.com>
2022-12-03 18:14:40 -0300

    Esta es la primer version del parcial de herramientas que contiene errores den la palabra instructor y no contiene la funicion EnActividad

---
+++ HerramientasPP_v1/Pages/InstructoresVueloPP/Detalle.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PrimerParcial.Models;
+using PrimerParcial.Servicios;
+
+namespace ProyHerramientas.Pages.Profesores
+{
+    public class DetalleModel : PageModel
+    {
+        [BindProperty]
+        public Instructor InstructorDetalle{get;set;}
+        private IInstructorServicio _instSrv;
+        public DetalleModel(IInstructorServicio instSrv){
+            _instSrv=instSrv;
+        }
+
+        public void OnGet(int id)
+        {
+            InstructorDetalle=_instSrv.MostrarTodos()
+                    .Where(x=>x.Id==id)
+                    .First();
+        }
+    }
+}
