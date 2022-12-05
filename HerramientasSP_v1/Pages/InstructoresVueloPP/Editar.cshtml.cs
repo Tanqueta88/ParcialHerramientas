6ee66cdc3dffdf63e227492239b322a57a2a866c
tanqueta88 <oliverprim783@gmail.com>
2022-12-04 23:23:39 -0300

    Se agrega la carpeta SPv1 para comenzar a trabajar la coneccion a DB de la aplicacion

---
+++ HerramientasSP_v1/Pages/InstructoresVueloPP/Editar.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PrimerParcial.Models;
+using PrimerParcial.Servicios;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PrimerParcial.Pages.InstructoresVueloPP
+{
+    public class EditarModel : PageModel
+    {
+        [BindProperty]
+        public Instructor InstructorEdit {get;set;}
+
+        private IInstructorServicio _instServicio;
+        public EditarModel(IInstructorServicio instrv){
+            _instServicio=instrv;
+        }
+        public void OnGet(int id)
+        {
+            var lista= _instServicio.MostrarTodos();
+            InstructorEdit=lista.Where(x=>x.Id==id).First();
+        }
+        public IActionResult OnPost(){
+            if(ModelState.IsValid){
+                _instServicio.Modificar(InstructorEdit);
+                return RedirectToPage("Listado");
+            }
+           else{
+            return Page();
+           }
+        }
+    }
+}
+    
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ParcialHerramientas
 En este repositorio va a estar el segundo parcial de herramientas de programacion
+PPv3= Contine lo indicado par el primer parcial (crear editar listado) mas funciones (a definir segun integracion a DB) y correcciones de volver a listado (crear) y nombre de variables
+
+SPv1= contiene la coneccion a BD (Para ir trabajando)
