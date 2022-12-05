66a22c2fe0d0987ee796598cdb6d77315dfbeb1c
tanqueta88 <oliverprim783@gmail.com>
2022-12-05 18:58:37 -0300

    Se suma la logica para el desploegable Hangar en la pantalla CREAR

--- HerramientasPP_v3/Pages/InstructoresVueloPP/Crear.cshtml.cs
+++ HerramientasPP_v3/Pages/InstructoresVueloPP/Crear.cshtml.cs
@@ -10,12 +10,24 @@ namespace PrimerParcial.Pages
     {
         [BindProperty]
         public Instructor InstructorIng {get;set;}
+        public List<SelectListItem> HangaresLista {get;set;}
          private IInstructorServicio _instrv;
-        public CrearModel(IInstructorServicio srv){
+         private IHangarServicio _hanSrv;
+         
+        public CrearModel(IInstructorServicio srv, IHangarServicio hanSrv){
             _instrv=srv;
+            _hanSrv=hanSrv;
         }
-        public void OnGet()
+        public void OnGet(int id)
         {
+            var lista= _instrv.MostrarTodos();
+            // InstructorIng=lista.Where(x=>x.Id==id).First();
+            HangaresLista=_hanSrv.MostrarHangares().Select(a=>
+                               new SelectListItem{
+                                    Value=a.Id.ToString(),
+                                    Text=a.Detalle
+                                }
+                            ).ToList();
         }
 
         public IActionResult OnPost(){
